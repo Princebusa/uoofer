cb59963ae4381b45ccd01ed3d609f0a05a92377a
Prince Busa <130892399+Princebusa@users.noreply.github.com>
2025-12-02 11:09:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import Header from "./component/header"
-import Hero from './component/home/hero'
+import Header from "./component/header";
+import Hero from "./component/home/hero";
+import { BrowserRouter, Router,Route, Routes } from "react-router-dom";
+import bg from "./assets/Hypro.jpg";
 function App() {
-
-
   return (
     <>
-    <Header/>
-      <Hero/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <>
+            <Header/>
+            <Hero />
+            </>
+          } />
+          <Route path="hypro" element={
+            <>
+             <img src={bg}/>
+            </>
+          }/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
